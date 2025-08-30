027ad7a6bc4b2244dbb74e5e9d16e91d81d864b1
Suraj Maxify <surajsingh@maxifysolution.com>
2025-08-30 17:33:57 +0530

    Update adminRoute.js

--- backend/src/routes/adminRoute.js
+++ backend/src/routes/adminRoute.js
@@ -1,14 +1,17 @@
 import express from 'express';
-import { deleteUserById, getAllUsers, getAllWithdrawals, getUserDetailById, loginAdmin, updateWithdrawalStatus } from '../controller/adminController.js';
+import { deleteUserById, getAllUsers, getAllWithdrawals, getUserDetailById, loginAdmin, setelMantsAmount, setelMantsAmountSingleUser, updateWithdrawalStatus } from '../controller/adminController.js';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/admin-login', loginAdmin);
 router.get('/get-all-users', authMiddleware, getAllUsers)
+router.put('/update-user-status/:id', authMiddleware, updateUserStatus)
 router.get('/get-user-by-id/:id', authMiddleware, getUserDetailById)
 router.delete('/delete-user-by-id/:id', authMiddleware, deleteUserById)
 router.get('/get-all-withdrawals', authMiddleware, getAllWithdrawals)
 router.put('/update-withdrawal-status/:id', authMiddleware, updateWithdrawalStatus)
+router.post('/user-setelments', setelMantsAmount)
+router.post('/sigle-user-setelments/:userId', setelMantsAmountSingleUser)
 
 export default router;
